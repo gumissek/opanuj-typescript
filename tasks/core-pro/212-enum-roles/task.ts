b171cb55c7c8fc2dbe966b9f3919e5a52ac018c5
Piotr Biliński <bilinski.piotr89@gmail.com>
2026-02-07 14:39:50 +0100

    zadanie 212-enum-roles

--- tasks/core-pro/212-enum-roles/task.ts
+++ tasks/core-pro/212-enum-roles/task.ts
@@ -1,13 +1,22 @@
-export enum UserPermission {}
-
-export enum UserRole {}
-
-export interface User {
-  role: string;
-  permissions: string[];
+export enum UserPermission {
+  VIEW, 
+  READ,
+  WRITE,
+  DELETE 
 }
 
-export function hasAccess(user: User, requiredPermission: string): boolean {
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  EDITOR = 'EDITOR',
+  VIEWER = 'VIEWER'
+}
+
+export interface User {
+  role: UserRole;
+  permissions: Array<UserPermission>;
+}
+
+export function hasAccess(user: User, requiredPermission: UserPermission): boolean {
   if (user.role === 'ADMIN') return true;
   return user.permissions.includes(requiredPermission);
 }
